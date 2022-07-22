ea3927eac7832f97ee1e06831e79a77cf8462662
renanconstancio <renanhconstancio@gmail.com>
2022-07-22 00:02:49 -0300

    push all

---
+++ src/config/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from "firebase/compat";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6Bzu1bC-Mbp-N0IN8kRGef_5U8TvX3eA",
+  authDomain: "rocket-help-b9610.firebaseapp.com",
+  projectId: "rocket-help-b9610",
+  storageBucket: "rocket-help-b9610.appspot.com",
+  messagingSenderId: "669971153637",
+  appId: "1:669971153637:web:dfd0fce1168e7ad9ec8a0c",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.firestore();
+export default database;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,12 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import { Loading } from "../components/Loading";
 // import { SignIn } from "../screens/SignIn";
-import database from "../config/firebase";
+// import database from "../config/firebase";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 // import { AppRoutes } from "./app.routes";
 
@@ -12,11 +17,11 @@ export function Routes() {
   const [user, setUser] = useState({});
 
   useEffect(() => {
-    // const subscriber = database.app.auth().onAuthStateChanged((resp) => {
-    //   setUser(resp);
-    //   setIsLoading(false);
-    // });
-    // return subscriber;
+    const subscriber = onAuthStateChanged(getAuth(), async (resp) => {
+      setUser(resp);
+      setIsLoading(false);
+    });
+    return subscriber;
   }, []);
 
   if (loading) {
